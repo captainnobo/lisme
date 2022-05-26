b32fc0f509320ded670b6ebec6bba7bf52db1506
Dr.Ra <theara2048@gmail.com>
2022-05-26 14:26:37 +0700

    update web page from office pc

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {FaLinux , FaWindows ,FaApple} from 'react-icons/fa'
+import { Button } from 'antd';
+import 'antd/dist/antd.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='header-title'>Lisme Free Audio Book Software</div>
+      <div>
+        <div className='main-title'>
+          Lisme AudioBook
+        </div>
+        <div className='main-subtitle'>
+          Latest Release
+          <span style={{marginLeft:10,marginRight:10}}>
+            <FaWindows/>
+            <FaApple />
+            <FaLinux />
+          </span> 
+          27.24 -March 29th
+        </div>
+        <div style={{marginTop:20}}>
+          <Button style={{marginLeft:10,marginRight:10}} size={'large'} type="primary">Windows</Button>
+          <Button style={{marginLeft:10,marginRight:10}} size={'large'} type="primary">macOS 10.13+</Button>
+          <Button style={{marginLeft:10,marginRight:10}} size={'large'} type="primary">Linux</Button>
+        </div>
+      </div>
+      <div style={{marginTop:30}}>
+        <img height={700} src='lisme/linux.png' />
+      </div>
     </div>
   );
 }
