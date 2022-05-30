691c09ed5949c7a6b212fc1e1c8ef9b4b70b1526
Dr.Ra <theara2048@gmail.com>
2022-05-30 13:27:26 +0700

    create linux download btn

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import {FaLinux , FaWindows ,FaApple} from 'react-icons/fa'
-import { Button } from 'antd';
+import { Button ,Tooltip} from 'antd';
 import 'antd/dist/antd.css';
 function App() {
   return (
@@ -21,9 +21,15 @@ function App() {
           27.24 -March 29th
         </div>
         <div style={{marginTop:20}}>
-          <Button style={{marginLeft:10,marginRight:10}} size={'large'} type="primary">Windows</Button>
-          <Button style={{marginLeft:10,marginRight:10}} size={'large'} type="primary">macOS 10.13+</Button>
-          <Button style={{marginLeft:10,marginRight:10}} size={'large'} type="primary">Linux</Button>
+          <Tooltip title={'Coming soon..!'}>
+            <Button disabled style={{marginLeft:10,marginRight:10}} size={'large'} type="primary">Windows</Button>
+          </Tooltip>
+          <Tooltip title={'Coming soon..!'}>
+            <Button disabled style={{marginLeft:10,marginRight:10}} size={'large'} type="primary">macOS 10.13+</Button>
+          </Tooltip>
+          <a href='https://github.com/captainnobo/lisme/releases/download/beta/Lisme-beta-0.1.0_linux.zip' target="_blank" rel='nofollow'>
+            <Button style={{marginLeft:10,marginRight:10}} size={'large'} type="primary">Linux</Button>
+          </a>
         </div>
       </div>
       <div style={{marginTop:30}}>
